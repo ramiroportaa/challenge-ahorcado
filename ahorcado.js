c0db48a1f55c1e8d0f2ceeddddb9227d6588aea3
ramiporta <p0rt4csd@gmail.com>
2022-02-04 02:35:51 -0300

    Logica del juego

--- ahorcado.js
+++ ahorcado.js
@@ -2,7 +2,9 @@ const vistaInicio = document.getElementById("inicio");
 const vistaAgregarPalabra = document.getElementById("agregar-palabra");
 const vistaJuego = document.getElementById("juego");
 
-var palabras = ["juego", "oracle", "one", "alura", "ahorcado"];
+var palabras = ["JUEGO", "ORACLE", "ONE", "ALURA", "AHORCADO"];
+
+const regex = new RegExp("^[A-Z\\s]+$");
 
 function mostrar (vista){
     vistaInicio.classList.add("display-none");
@@ -24,16 +26,55 @@ function mostrarGuiones (palabra){
     for (let i = 0; i < cantidad; i++) {
         const letra = palabra[i];
         word.innerHTML += `<div class="letter">
-                                <p id="${letra}" class="display-none">${letra}</p>
+                                <p class="${letra} display-none">${letra}</p>
                                 <img src="img/underline.png">
                             </div>`;
     };
 }
 
+function mostrarLetraCorrecta(letra) { 
+    const letter = document.querySelectorAll(`.${letra}`);
+    letter.forEach(letra =>(
+        letra.classList.remove("display-none")
+    ));
+}
+
+function escribirLetraIncorrecta(letra) { 
+    const other = document.querySelector(".other");
+    other.innerHTML += `<p>${letra}</p>`
+}
+
+function estaEnPalabra(letra, palabra) {
+    return palabra.includes(letra);
+}
+
+function verificarLetra(letra) {
+    return regex.test(letra);
+}
+
 function IniciarJuego (){
+    let errores = [];
+    let letrasCorrectas = "";
     const secreto = escogerPalabra(palabras);
     mostrarGuiones(secreto);
-
+    document.addEventListener("keyup", (e)=>{
+        if (verificarLetra(e.key)) {
+            let letra = e.key;
+            if (estaEnPalabra(letra, secreto)) {
+                if (!letrasCorrectas.includes(letra)) {
+                    letrasCorrectas += letra;
+                    mostrarLetraCorrecta(letra);
+                }
+            }else{
+                if (!errores.includes(letra)) {
+                    errores.push(letra);
+                    escribirLetraIncorrecta(letra);
+                }
+            }
+        }else{
+            //Mostrar algo cuando no pasa el REGEX
+        }
+    })
     mostrar(vistaJuego);
 }
 
